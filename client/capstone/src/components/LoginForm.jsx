7f7524aada5478437c1aed1b27e9dc1701bbf802
frank-nn <franknguyen@outlook.com.au>
2025-03-23 22:37:41 +1100

    login Validation

--- client/capstone/src/components/LoginForm.jsx
+++ client/capstone/src/components/LoginForm.jsx
@@ -1,28 +1,58 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Validation from "./LoginValidation";
 
 function LoginForm() {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+  const handleInput = (event) =>
+    setValues((prev) => ({
+      ...prev,
+      [event.target.name]: [event.target.value],
+    }));
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(Validation(values));
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
       <div className="bg-white p-3 rounded w-25">
-        <form action="">
+        <h2>Sign-In</h2>
+        <form action="" onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="email">Email</label>
             <input
               type="email"
               placehollder="Enter Email"
+              name="email"
+              onChange={handleInput}
               className="form-control rounded-0"
             />
+            {errors.email && (
+              <span className="text-danger"> {errors.email}</span>
+            )}
           </div>
           <div className="mb-3">
-            <label htmlFor="email">Password</label>
+            <label htmlFor="password">Password</label>
             <input
-              type="email"
+              type="password"
               placehollder="Enter Password"
+              name="password"
+              onChange={handleInput}
               className="form-control rounded-0"
             />
+            {errors.password && (
+              <span className="text-danger"> {errors.password}</span>
+            )}
           </div>
-          <button className="btn btn-success w-100">Log in </button>
+          <button type="submit" className="btn btn-success w-100">
+            Log in{" "}
+          </button>
           <p> </p>
           <Link
             to="/Signup"
---
+++ client/capstone/src/components/LoginValidation.jsx
@@ -0,0 +1,29 @@
+function Validation(values) {
+  let error = {};
+
+  const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
+
+  // Email Validation
+  if (!values.email) {
+    error.email = "Email should not be empty.";
+  } else if (!email_pattern.test(values.email)) {
+    error.email = "Invalid email format.";
+  } else {
+    error.email = "";
+  }
+
+  // Password Validation
+  if (!values.password) {
+    error.password = "Password should not be empty.";
+  } else if (!password_pattern.test(values.password)) {
+    error.password =
+      "Password must be 8 characters long, contain at least one uppercase, one lowercase, one number, and one special character.";
+  } else {
+    error.password = "";
+  }
+
+  return error;
+}
+
+export default Validation;
