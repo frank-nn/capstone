a0b4be0354255aa1543b6b36c5d093e2a3ca681b
frank-nn <franknguyen@outlook.com.au>
2025-03-29 11:30:46 +1100

    Connection to database

--- server/index.js
+++ server/index.js
@@ -1,34 +1,18 @@
 const express = require("express");
-const mysql = require("mysql");
-const cors = require("cors");
-
 const app = express();
-app.use(cors());
+require("dotenv").config(); // Make sure to load environment variables from .env
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "signup",
+let dbConnect = require("./lib/dbConnect");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my MySQL application." });
 });
 
-// const express = require("express");
-// const app = express();
-// require("dotenv").config();
-// let dbConnect = require("./lib/dbConnect");
-
-// // parse requests of content-type - application / json;
-
-// app.use(express.json());
-// //
-
-// app.get("/", (req, res) => {
-//   res.json({ message: "Welcome to my express backend!!!" });
-// });
-
-// // set port, listen for requests
-// const PORT = process.env.PORT || 8080;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port
-// ${PORT}.`);
-// });
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
