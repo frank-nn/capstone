6c36d9c20632b6c22fdba0a37776018bb875d8c3
frank-nn <franknguyen@outlook.com.au>
2025-04-03 20:22:58 +1100

    other bars added

---
+++ client/capstone/src/components/feed/feed.jsx
@@ -0,0 +1,5 @@
+import "./Feed.css"; // Corrected import
+
+export default function Feed() {
+  return <div className="feed">Feed!!</div>; // Added a class for styling
+}
--- client/capstone/src/components/home.jsx
+++ client/capstone/src/components/home.jsx
@@ -1,11 +1,16 @@
 import Topbar from "./topbar/Topbar.jsx";
 import Sidebar from "./sidebar/Sidebar.jsx";
+import Rightbar from "./rightbar/rightbar.jsx";
+import Feed from "./feed/Feed.jsx";
 
 export default function Home() {
   return (
     <>
       <Topbar />
+      <div className="homeContainer"></div>
       <Sidebar />
+      <Rightbar />
+      <Feed />
     </>
   );
 }
