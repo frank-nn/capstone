3aaa2d2d7f224b0c5ca9c33ec8e08f3b450e8405
frank-nn <franknguyen@outlook.com.au>
2025-04-05 17:20:30 +1100

    post edit

--- client/capstone/src/components/feed/feed.jsx
+++ client/capstone/src/components/feed/feed.jsx
@@ -1,11 +1,13 @@
 import "./feed.css"; // Corrected import
 import Share from "../share/Share";
+import Post from "../post/Post";
 
 export default function Feed() {
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
+        <Post />
       </div>
     </div>
   ); // Added a class for styling
---
+++ client/capstone/src/components/post/Post.jsx
@@ -0,0 +1,29 @@
+import "./post.css";
+import { MoreVert } from "@mui/icons-material";
+
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              className="postProfileImg"
+              src="/assets2/person/1.PNG"
+              alt=""
+            />
+            <span className="postUserName">Frank</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">Hey! its my first post :) </span>
+          <img className="postImg" src="/assets2/post/1.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
